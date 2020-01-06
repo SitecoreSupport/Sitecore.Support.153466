30ff9d5b9a6781c107a680eb344f763c8d84bde5
OlegBraginskyy <obr@sitecore.net>
2020-01-06 10:12:44 +0200

    Init code

---
+++ src/Sitecore.Support.153466/GetInheritedValue.cs
@@ -0,0 +1,25 @@
+﻿namespace Sitecore.Support.Pipelines.GetFieldValue
+{
+    using Sitecore;
+    using Sitecore.Diagnostics;
+    using Sitecore.Pipelines.GetFieldValue;
+    using System;
+
+    public class GetInheritedValue
+    {
+        public void Process(GetFieldValueArgs args)
+        {
+            if (args.AllowInheritValue)
+            {
+                string inheritedValue = args.Field.GetInheritedValue();
+                if (inheritedValue != null)
+                {
+                    args.InheritsValueFromOriginalItem = true;
+                    args.Value = inheritedValue;
+                    args.AbortPipeline();
+                }
+            }
+
+        }
+    }
+}
